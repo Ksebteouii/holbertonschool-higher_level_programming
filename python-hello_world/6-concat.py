e6c2ada8ab322e563ffc7d061f737b0b2c0412b1
sebteouii <sebteouiikhoubayb@gmail.com>
2024-01-28 13:49:07 +0100

    Welcome to Holberton School!

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str = "object-oriented programming with Python"
-print(str[39:-63] + str[49:-42] + str[:6] + '\n', end="")
+str1 = "Welcome to"
+str2 = "Holberton School!"
+print(str1 + " " + str2)
