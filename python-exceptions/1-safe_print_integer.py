23360b2528755a41d5828331a9ff399cb0fb80bd
sebteouii <sebteouiikhoubayb@gmail.com>
2024-02-10 22:57:55 +0100

    function that prints an integer with {:d}.format()

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    j = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            j += 1
-        except (TypeError, ValueError):
-            continue
-    print()
-    return j
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
