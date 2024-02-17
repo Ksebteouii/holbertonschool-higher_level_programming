1f0193f13828885907be78c0cf755f99d1acfbd2
sebteouii <sebteouiikhoubayb@gmail.com>
2024-02-17 15:33:01 +0100

    stdout the Rectangle instance with the character #

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -68,3 +68,8 @@ class Rectangle(Base):
     def area(self):
         """compute and return the area of the rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """Print the rectangle with '#' characters"""
+        for _ in range(self.height):
+            print("#" * self.width)
