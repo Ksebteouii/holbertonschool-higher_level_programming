9f044490eb63ba3bad001e40f54f5ed6d4def832
sebteouii <sebteouiikhoubayb@gmail.com>
2024-02-17 18:00:18 +0100

    ///

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,11 +6,8 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """A class representing a rectangle that inherits from the Base class"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a Rectangle object with the specified
-        width, height, x, y, and optional id"""
+
         self.width = width
         self.height = height
         self.x = x
@@ -19,11 +16,13 @@ class Rectangle(Base):
 
     @property
     def width(self):
+
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -31,11 +30,13 @@ class Rectangle(Base):
 
     @property
     def height(self):
+
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -43,32 +44,38 @@ class Rectangle(Base):
 
     @property
     def x(self):
+
         return self.__x
 
     @x.setter
     def x(self, value):
-        if type(value) is not int:
+
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+
         return self.__y
 
     @y.setter
     def y(self, value):
-        if type(value) is not int:
+
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
-        self.__width * self.__height
+
+        return self.__width * self.__height
 
     def display(self):
+
         print("{}".format("\n" * self.__y), end="")
         for i in range(self.height):
             print("{}{}".format(" " * self.__x, "#" * self.width))
