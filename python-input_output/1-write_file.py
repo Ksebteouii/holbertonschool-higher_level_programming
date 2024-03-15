b3ef09f12cb9a2aa70c711cd04061a0de0ce923a
sebteouii <sebteouiikhoubayb@gmail.com>
2024-03-15 16:36:56 +0100

    Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Writes text to a UTF-8 encoded file."""
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="UTF-8") as f:
+    """
+    Writes text to a UTF-8 encoded file and returns the number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
